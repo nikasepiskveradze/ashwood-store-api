fa37a395a3e950c9f6db3f3cb250745b2d1474f5
Nika Sepiskveradze <nika.sepiskveradze@gmail.com>
2023-09-08 01:08:08 +0400

    Connected postgres and some basic setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,41 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import * as process from 'process';
+// import { User } from './users/user.entity';
+import { APP_PIPE } from '@nestjs/core';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: 'postgres',
+          host: configService.get<string>('DATABASE_HOST'),
+          database: configService.get<string>('DATABASE_NAME'),
+          port: +configService.get<number>('DATABASE_PORT'),
+          username: configService.get<string>('DATABASE_USERNAME'),
+          password: configService.get<string>('DATABASE_PASSWORD'),
+          entities: [],
+          synchronize: true,
+        };
+      },
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({ whitelist: true }),
+    },
+  ],
 })
 export class AppModule {}
